a38349867ef6bf60a551823a2047e5ecbceda4ae
위성훈 <hun12345689@gmail.com>
2026-07-24 14:27:50 +0900

    chore: application.yml 프로파일 분리 (local/prod)

---
+++ src/main/java/com/seeat/seeatapi/SeeatApiApplication.java
@@ -0,0 +1,13 @@
+package com.seeat.seeatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeatApiApplication.class, args);
+    }
+
+}
