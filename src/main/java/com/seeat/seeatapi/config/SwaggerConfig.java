e591f489b6db569c2534196d7063bf4e4fc3910d
위성훈 <hun12345689@gmail.com>
2026-07-24 20:23:49 +0900

    feat: JPA Auditing 활성화

---
+++ src/main/java/com/seeat/seeatapi/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.seeat.seeatapi.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo())
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components().addSecuritySchemes(
+                        SECURITY_SCHEME_NAME,
+                        new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                ));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("SEEAT API")
+                .description("수산물 거래 플랫폼 SEEAT의 REST API 명세 (v2.1)")
+                .version("v2.1");
+    }
+}
